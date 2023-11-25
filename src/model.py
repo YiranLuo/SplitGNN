909d366603db7a62b73c878909884f3b180473fb
YiranLuo <yiranluo1210@gmail.com>
2023-11-24 19:49:57 -0800

    Modify Siamese Neural Network

--- src/model.py
+++ src/model.py
@@ -201,15 +201,6 @@ class MyRelationAwareSiameze(nn.Module):
         return torch.sum(torch.abs(output1 - output2), dim=1) # Manhattan Distance (L1 norm)
         # return 1 - torch.corrcoef(output1, output2) # Pearson correlation
     
-class ContrastiveLoss(nn.Module):
-    def __init__(self, margin=1.0):
-        super().__init__()
-        self.margin = margin
-        
-    def forward(self, distance, label):
-        loss_contrastive = torch.mean((1-label) * torch.pow(distance, 2) +
-                                      (label) * torch.pow(torch.clamp(self.margin - distance, min=0.0), 2))
-        return loss_contrastive
 
 
 class MultiRelationSplitGNNLayer(nn.Module):
